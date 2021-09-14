15a8402d4043cb07f636c8911c45eb1dc6183dce
muhammadhamdits <muhammadhamdi702@gmail.com>
2021-09-14 10:40:13 +0700

    Adds code, type, and semester field into course

--- controllers/course.js
+++ controllers/course.js
@@ -2,7 +2,14 @@ const Course = require('../models/Course')
 const { errorHandling } = require('../database/utils')
 
 const index = async (req, res) => {
-  const courses = await Course.findAll()
+  const tempCourses = await Course.findAll()
+  let courses = []
+  tempCourses.forEach(tempCourse => {
+    let course = tempCourse.dataValues
+    if(course.type === 0) course.typeName = 'Required'
+    else course.typeName = 'Optional'
+    courses.push(course)
+  })
   res.json(courses)
 }
 
@@ -19,10 +26,12 @@ const create = async (req, res) => {
 const update = async (req, res) => {
   try {
     const id = req.params.courseId
+    // console.log(req.body)
     const course = await Course.update(req.body, { where: { id } })
     if(course[0] === 0) res.json({ error: `Failed to update data. Data not found or no changes submitted!` })
     else res.json({ success: "Successfully updates course data." })
   } catch (e) {
+    // console.log(e)
     const errorMessage = errorHandling(e)
     res.json(errorMessage)
   }
--- models/Course.js
+++ models/Course.js
@@ -7,10 +7,18 @@ class Course extends Model {
 
 Course.init(
   {
+    code: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: { msg: "This code already registered, please input another!" },
+      validate: { 
+        notEmpty: { msg: "Code cannot be empty, please input this field!" },
+        notNull: { msg: "Code cannot be empty, please input this field!" }
+      }
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: { msg: "This name already registered, please input another!" },
       validate: { 
         notEmpty: { msg: "Name cannot be empty, please input this field!" },
         notNull: { msg: "Name cannot be empty, please input this field!" }
@@ -24,6 +32,26 @@ Course.init(
         notNull: { msg: "SKS cannot be empty, please input this field!" },
         isInt: { msg: "SKS must be integer value!" },
         min: { args: 1, msg: "SKS invalid, sks mininum value is 1!" }
+      }
+    },
+    type: {
+      type: DataTypes.INTEGER, // 0: Wajib, 1: Pilihan
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: "Type cannot be empty, please input this field!" },
+        notNull: { msg: "Type cannot be empty, please input this field!" },
+        isInt: { msg: "Type must be valid value!" }
+      }
+    },
+    semester: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: "Type cannot be empty, please input this field!" },
+        notNull: { msg: "Type cannot be empty, please input this field!" },
+        isInt: { msg: "Type must be valid value!" },
+        min: { args: 1, msg: "SKS invalid, sks mininum value is 1!" },
+        max: { args: 8, msg: "SKS invalid, sks maximum value is 1!" }
       }
     }
   },
